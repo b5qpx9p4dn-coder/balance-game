2edd33b4a842903815245360020ae634f533d48b
Seungyong <tmddyd5604@naver.com>
2026-05-15 09:09:32 +0900

    community project update

--- app.py
+++ app.py
@@ -1,33 +1,18 @@
-from flask import Flask, render_template, request, redirect, session
-from db import get_db
+from flask import Flask, render_template, request, redirect, session, flash
+from db import get_db, init_db
 
 
 app = Flask(__name__)
 app.secret_key ="testkey"
+
+init_db()
+
 @app.route("/")
 def home():
 
     db = get_db()
 
-    db.execute("""
-    CREATE TABLE IF NOT EXISTS posts (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        title TEXT,
-        option_a TEXT,
-        option_b TEXT,
-        vote_a INTEGER DEFAULT 0,
-        vote_b INTEGER DEFAULT 0
-    )
-    """)
-    
-    db.execute("""
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT UNIQUE,
-        password TEXT
-    )
-    """)
-    db.commit()
+ 
 
     posts = db.execute("SELECT * FROM posts").fetchall()
 
@@ -38,8 +23,8 @@ def home():
 def write():
 
     if "user_id" not in session:
-        return redirect("/login")
-
+        flash("로그인이 필요합니다.")
+        return redirect("/")
     if request.method =="POST":
         title = request.form["title"]
         option_a = request.form["option_a"]
@@ -76,24 +61,26 @@ def vote(post_id, option):
         """,(post_id,))
     db.commit()
 
-    return redirect("/")
+    return redirect(f"/post/{post_id}")
 
 
 @app.route("/edit/<int:post_id>", methods =["GET","POST"])
 def edit(post_id):
-
-    if "user_id" not in session:
-        return "로그인 필요"
-
     db=get_db()
+    if "user_id" not in session:
+        flash("로그인이 필요합니다.")
+        return redirect("/login")
 
+    
     post = db.execute("""
         SELECT * FROM posts WHERE id = ?
     """, (post_id,)).fetchone()
     if post is None: 
-        return "글 없음"
+        flash("글이 없습니다.")
+        return redirect("/")
     if post["user_id"] != session.get("user_id"):
-        return "권한 없음"
+        flash("권한이 없습니다.")
+        return redirect("/")
 
     if request.method == "POST":
 
@@ -108,25 +95,31 @@ def edit(post_id):
         """, (title, option_a, option_b, post_id))
 
         db.commit()
-
+        flash("수정 완료")
         return redirect("/")
     
 
     return render_template("edit.html", post = post)
 @app.route("/delete/<int:post_id>", methods =["POST"])
 def delete(post_id):
-    if "user_id" not in session:
-        return "로그인 필요"
     db=get_db()
+ 
+    if "user_id" not in session:
+        flash("로그인이 필요합니다.")
+        return redirect("/login")
+  
     post = db.execute("""
         SELECT * FROM posts WHERE id =?
     """,(post_id,)).fetchone()
 
     if post is None:
-        return "글 없음"
+        flash("글 없음")
+        return redirect("/")
+
 
     if post["user_id"] != session.get("user_id"):
-        return "권한 없음"
+        flash("권한이 없습니다.")
+        return redirect("/")
 
         
 
@@ -135,7 +128,9 @@ def delete(post_id):
         WHERE id = ?
     """,(post_id,))
     db.commit()
+    flash("삭제 완료")
     return redirect("/")
+
   
    
 
@@ -145,8 +140,26 @@ def register():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
+        password2 = request.form["password2"]
+
+        if not username or not password or not password2:
+            flash("모든 항목을 입력해주세요")
+            return render_template("register.html", username = username)
+
+        if password != password2:
+            flash("비밀번호가 일치하지 않습니다")
+            return redirect("/register")
 
         db = get_db()
+
+        user = db.execute("""
+            SELECT * FROM users
+            WHERE username = ?
+        """,(username,)).fetchone()
+
+        if user:
+            flash(" 이미 존재하는 아이디입니다.")
+            return redirect("/register")
 
         db.execute("""
             INSERT INTO users (username, password)
@@ -154,7 +167,7 @@ def register():
         """,(username, password))
 
         db.commit()
-
+        flash("화원가입 완료")
         return redirect("/login")
     return render_template("register.html")
 
@@ -174,7 +187,8 @@ def login():
             session["user_id"] = user["id"]
             session["username"] = user["username"]
             return redirect("/")
-        return "로그인 실패"
+        flash("아이디 혹은 비밀번호가 틀렸습니다.")
+        return redirect("/login")
     return render_template("login.html")
 @app.route("/logout")
 def logout():
@@ -198,13 +212,41 @@ def post_detail(post_id):
         percent_b = 100 - percent_a
     else:
         percent_a = percent_b = 0
+
+    comments = db.execute("""
+        SELECT comments.*, users.username
+        FROM comments
+        JOIN users ON comments.user_id = users.id
+        WHERE post_id = ?
+        ORDER BY comments.id DESC
+
+    """,(post_id,)).fetchall()
     
     return render_template(
         "detail.html", 
         post=post,
         percent_a = percent_a,
         percent_b = percent_b,
-        total=total
+        total=total,
+        comments=comments
     )
+
+
+@app.route("/comment/<int:post_id>", methods=["POST"])
+def comment(post_id):
+    if "user_id" not in session:
+        flash("로그인이 필요합니다.")
+        return redirect(f"/post/{post_id}")
+    content = request.form["content"]
+
+    db = get_db()
+    db.execute("""
+        INSERT INTO comments (content, post_id, user_id)
+        VALUES (?,?,?)
+    """,(content, post_id, session["user_id"]))
+    db.commit()
+    return redirect(f"/post/{post_id}")
+
 if __name__ == "__main__":
     app.run(debug=True)
+
--- db.py
+++ db.py
@@ -4,3 +4,38 @@ def get_db():
     conn = sqlite3.connect("balance.db")
     conn.row_factory = sqlite3.Row
     return conn
+def init_db():
+
+    db=get_db()
+
+    db.execute("""
+    CREATE TABLE IF NOT EXISTS posts (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        option_a TEXT,
+        option_b TEXT,
+        vote_a INTEGER DEFAULT 0,
+        vote_b INTEGER DEFAULT 0,
+        user_id INTEGER
+    )
+    """)
+    
+    db.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE,
+        password TEXT
+    )
+    """)
+
+    db.execute("""
+    CREATE TABLE IF NOT EXISTS comments(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        content TEXT,
+        post_id INTEGER,
+        user_id INTEGER,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )          
+    """)
+    db.commit()
+    db.close()
